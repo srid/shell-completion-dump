7d6e98700b73b6ce2027c33dab23f86148c4241b
Sridhar Ratnakumar <srid@srid.ca>
2022-12-20 12:45:58 -0500

    Okay, but not very useful, due to --index/--word reliance

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # shell-completion-dump
 
 
+Run `nix build` and then:
+
+```sh-session
+$ ./result/bin/shell-completion-dump --bash-completion-script $PWD/result/bin/shell-completion-dump
+```
+
+Repeat the same for fish, zsh.
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,26 @@
 module Main where
 
 import Main.Utf8 qualified as Utf8
+import Options.Applicative
 
-{- |
- Main entry point.
+data Sample
+  = Hello [String]
+  | Goodbye
+  deriving stock (Eq, Show)
 
- The `, run` script will invoke this function.
--}
+hello :: Parser Sample
+hello = Hello <$> many (argument str (metavar "TARGET..."))
+
+sample :: Parser Sample
+sample =
+  subparser
+    ( command "hello" (info hello (progDesc "Print greeting"))
+        <> command "goodbye" (info (pure Goodbye) (progDesc "Say goodbye"))
+    )
+
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
   Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+    execParser (info sample idm) >>= \case
+      Hello targets -> mapM_ putStrLn targets
+      Goodbye -> putStrLn "Goodbye!"
